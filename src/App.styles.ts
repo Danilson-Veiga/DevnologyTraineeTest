e4e47a5b6aa82e21f7aa0dbf06bacd0f6784f548
Danilson-Veiga <danilson.s.veiga@gmail.com>
2023-01-16 01:25:07 -0100

    feat: header

---
+++ src/App.styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  padding: 30px 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+
+  h1,
+  h3 {
+    margin: 0;
+  }
+`;
---
+++ src/components/table/table.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { TableContainer } from "./table.styles";
+
+export const Table: FC = () => {
+  return <TableContainer></TableContainer>;
+};
